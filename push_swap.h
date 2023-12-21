72957544f49146553e9d9049a8030522c330a445
MezyanM50 <mezyanm50@gmail.com>
2023-12-21 17:41:35 +0100

    push_swap

--- push_swap.h
+++ push_swap.h
@@ -1,8 +1,16 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+
+typedef struct s_stack
+{
+	int	value;
+	int	index;
+	struct s_stack *next;
+}	t_stack
+
 # include <stdio.h>
 # include "printf/ft_printf.h"
-#include "libft/libft.h"
+# include "libft/libft.h"
 
 #endif
